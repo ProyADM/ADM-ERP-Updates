7443d0bc7548d77385d2fb08b56d72a0d0d0103b
ProyADM <proyectos.adm.sid@gmail.com>
2026-08-29 11:48:20 -0300

    chore: reset base a version 1.0.0 con instalador corregido
    - Nuevo instalador 1.0.0 con Dashboard, notificaciones y correcciones
    - version.json actualizado a 1.0.0 con changelog limpio
    - Eliminados archivos obsoletos de la version 1.0.1
    - Instalador con clave de registro y acceso directo corregidos

--- updates/1.0.1/files/modules/cotizaciones/__init__.py
+++
@@ -1,3 +0,0 @@
-from .routes import cotizaciones_bp
-
-__all__ = ['cotizaciones_bp']
--- updates/1.0.1/files/modules/shared/__init__.py
+++
@@ -1,8 +0,0 @@
-from .database import run_sql, run_sql_db, _sql_literal, get_db_context, set_contexto_base
-from .utils import to_base64, normalizar_fecha, limpiar_texto
-from .routes import shared_bp
-
-__all__ = [
-    'run_sql', 'run_sql_db', '_sql_literal', 'get_db_context', 'set_contexto_base',
-    'to_base64', 'normalizar_fecha', 'limpiar_texto', 'shared_bp'
-]
