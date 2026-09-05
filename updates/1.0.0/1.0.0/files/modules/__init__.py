a8caf8730e7806ecd15410beb5aede425d2d406e
ProyADM <proyectos.adm.sid@gmail.com>
2026-09-05 13:54:50 -0300

    Versión base 1.0.0 (estructura limpia, sin archivos grandes)

--- updates/1.0.0/1.0.0/files/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-# Módulo principal
-from . import stock
-from . import cxp
-from . import cotizaciones
-from . import shared
-
-__all__ = ['stock', 'cxp', 'cotizaciones', 'shared']
--- updates/1.0.0/1.0.0/files/modules/reportes/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# modules/reportes/services/__init__.py
-from . import contratos_service
-from . import pendiente_cobro_service
-from . import consolidado_pais_service
-from . import consolidado_total_service
-from . import ventas_manuales_service
